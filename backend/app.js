cb1e0a273372650b9f937812a092a679614b3e6e
wiljha <wiljha@hotmail.com>
2021-11-21 19:40:40 -0500

    backend

---
+++ backend/app.js
@@ -0,0 +1,29 @@
+//importación del módulo express bajo el alias express
+const express = require('express')
+//genera una nueva instancia de express
+const app = express()
+//importación del mongoose
+const mongoose = require("mongoose");
+
+//Import de las rutas
+
+const postRoutes = require("./routes/posts")
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false}))
+
+//configuración de la conexión a la BASE DE DATOS
+////cadena de conexión, y un then que es cuando se ejecuta el connect que hace y un catch para capturar errores
+mongoose.connect(
+    "mongodb+srv://DBPizzaYa:XG0cfegZ0w5Z7jaf@cluster0.mxjuj.mongodb.net/PizzaYa?retryWrites=true&w=majority"
+    ).then(() => {
+        console.log("Estamos conectados a la DB...")
+    }).catch(() => {
+        console.log("Hay un problema con la conexión!")
+    });
+
+
+app.use("/Producto", postRoutes);
+
+//exportar app para que lo use en server.js
+module.exports =app;
---
+++ backend/controllers/posts.js
@@ -0,0 +1,52 @@
+//const producto = require("../models/producto");
+//modelo se importa, el nombre de la importación es Producto
+const Producto = require("../models/producto");
+
+//const posts = [
+//    {
+//      title: "Primer Post",
+//      summary: "Esto es un Post",
+//      content: "nuestro primer post desde el servidor"
+//    },
+//    {
+//      title: "Segundo Post",
+//      summary: "Esto es un Post",
+//      content: "felicitaciones"
+//    }
+//  ];
+
+exports.getMessage = (req, res) => {
+    res.send('Bienvenido a PizzaYa!');
+  };
+
+  //  **Debería retornar los post almacenados**, cuando llega peticiones a /api/posts, se envía un response utilzando un JSON
+//  **el JSON envía los posts, al response se le indica también un código de respuesta además del contenido que se envía
+
+
+exports.getProducto = (req, res) => {
+  //console.log("entró a la funcion getProducto");
+  //res.status(200).json("trabajando");
+  Producto.find({nombre: 'chorizo'}).then((resultado) => {
+    res.status(200).json(resultado);
+  });
+  };
+  
+
+  exports.addProducto = (req, res) => {
+      console.log(req.body);
+  //a la constante posts le agrega lo que llegó, registro en memoria
+  //posts.push(req.body); 
+  //incluir en la DB
+  const productoAdd = new Producto({
+    nombre: req.body.nombre,
+    precio: req.body.precio,
+    stock: req.body.stock,
+    descripcion: req.body.descripcion,
+  });
+
+  productoAdd.save().then((createdProducto) => {
+      console.log(createdProducto);
+      //respuesta 201 que es código de creado
+      res.status(201).json("Producto creado");
+  });
+  };
